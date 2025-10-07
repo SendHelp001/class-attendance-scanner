bb7ec0835933082768dc2ea2333b506a53c7850c
SendHelp001 <jonardsalanip@gmail.com>
2025-10-07 23:03:36 +0800

    feat: update project configuration for gh-pages deployment and enhance routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./utils/AuthProvider";
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter basename="/devcon-attendance">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // set base for gh-pages hosting (trailing slash is recommended)
+  base: '/devcon-attendance/',
   plugins: [
     react(),
     legacy()
